3ca0bed6e314727aeffa2279652f6f8353db6798
Surbhi <surbhiprasad31@gmail.com>
2023-02-06 21:43:41 +0530

    exerciseVideos completed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
   },
 };
 
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "01e85410c6msh69aa82e661c9cd3p1cae5cjsn334b9f0a7121",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
